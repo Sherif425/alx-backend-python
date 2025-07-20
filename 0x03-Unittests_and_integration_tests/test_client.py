dc1e629898ede95a195d0c75804d25f1393aeb8a
Sherif Mohamed <sherif425@gmail.com>
2025-07-20 11:54:19 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -52,6 +52,16 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_public_repos_url.assert_called_once()
             self.assertEqual(result, ["repo1", "repo2"])
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo: Dict[str, Any], license_key: str,
+                         expected: bool) -> None:
+        """Test GithubOrgClient.has_license returns expected boolean."""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
