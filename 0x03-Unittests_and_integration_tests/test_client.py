182d19fb6b794234e13acfbb839c7964103f79d5
Sherif Mohamed <sherif425@gmail.com>
2025-07-20 12:31:52 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -73,137 +73,168 @@ class TestGithubOrgClient(unittest.TestCase):
                 "id": 7697149,
                 "node_id": "MDEwOlJlcG9zaXRvcnk3Njk3MTQ5",
                 "name": "episodes.dart",
-                "full_name": "google/episodes.dart",
-                "private": False,
-                "owner": {
-                    "login": "google",
-                    "id": 1342004,
-                    "node_id": "MDEyOk9yZ2FuaXphdGlvbjEzNDIwMDQ=",
-                    "avatar_url": "https://avatars1.githubusercontent.com/u/1342004?v=4",
-                    "gravatar_id": "",
-                    "url": "https://api.github.com/users/google",
-                    "html_url": "https://github.com/google",
-                    "followers_url": "https://api.github.com/users/google/followers",
-                    "following_url": "https://api.github.com/users/google/following{/other_user}",
-                    "gists_url": "https://api.github.com/users/google/gists{/gist_id}",
-                    "starred_url": "https://api.github.com/users/google/starred{/owner}{/repo}",
-                    "subscriptions_url": "https://api.github.com/users/google/subscriptions",
-                    "organizations_url": "https://api.github.com/users/google/orgs",
-                    "repos_url": "https://api.github.com/users/google/repos",
-                    "events_url": "https://api.github.com/users/google/events{/privacy}",
-                    "received_events_url": "https://api.github.com/users/google/received_events",
-                    "type": "Organization",
-                    "site_admin": False
-                },
-                "html_url": "https://github.com/google/episodes.dart",
-                "description": "A framework for timing performance of web apps.",
-                "fork": False,
-                "url": "https://api.github.com/repos/google/episodes.dart",
-                "forks_url": "https://api.github.com/repos/google/episodes.dart/forks",
-                "keys_url": "https://api.github.com/repos/google/episodes.dart/keys{/key_id}",
-                "collaborators_url": "https://api.github.com/repos/google/episodes.dart/collaborators{/collaborator}",
-                "teams_url": "https://api.github.com/repos/google/episodes.dart/teams",
-                "hooks_url": "https://api.github.com/repos/google/episodes.dart/hooks",
-                "issue_events_url": "https://api.github.com/repos/google/episodes.dart/issues/events{/number}",
-                "events_url": "https://api.github.com/repos/google/episodes.dart/events",
-                "assignees_url": "https://api.github.com/repos/google/episodes.dart/assignees{/user}",
-                "branches_url": "https://api.github.com/repos/google/episodes.dart/branches{/branch}",
-                "tags_url": "https://api.github.com/repos/google/episodes.dart/tags",
-                "blobs_url": "https://api.github.com/repos/google/episodes.dart/git/blobs{/sha}",
-                "git_tags_url": "https://api.github.com/repos/google/episodes.dart/git/tags{/sha}",
-                "git_refs_url": "https://api.github.com/repos/google/episodes.dart/git/refs{/sha}",
-                "trees_url": "https://api.github.com/repos/google/episodes.dart/git/trees{/sha}",
-                "statuses_url": "https://api.github.com/repos/google/episodes.dart/statuses/{sha}",
-                "languages_url": "https://api.github.com/repos/google/episodes.dart/languages",
-                "stargazers_url": "https://api.github.com/repos/google/episodes.dart/stargazers",
-                "contributors_url": "https://api.github.com/repos/google/episodes.dart/contributors",
-                "subscribers_url": "https://api.github.com/repos/google/episodes.dart/subscribers",
-                "subscription_url": "https://api.github.com/repos/google/episodes.dart/subscription",
-                "commits_url": "https://api.github.com/repos/google/episodes.dart/commits{/sha}",
-                "git_commits_url": "https://api.github.com/repos/google/episodes.dart/git/commits{/sha}",
-                "comments_url": "https://api.github.com/repos/google/episodes.dart/comments{/number}",
-                "issue_comment_url": "https://api.github.com/repos/google/episodes.dart/issues/comments{/number}",
-                "contents_url": "https://api.github.com/repos/google/episodes.dart/contents/{+path}",
-                "compare_url": "https://api.github.com/repos/google/episodes.dart/compare/{base}...{head}",
-                "merges_url": "https://api.github.com/repos/google/episodes.dart/merges",
-                "archive_url": "https://api.github.com/repos/google/episodes.dart/{archive_format}{/ref}",
-                "downloads_url": "https://api.github.com/repos/google/episodes.dart/downloads",
-                "issues_url": "https://api.github.com/repos/google/episodes.dart/issues{/number}",
-                "pulls_url": "https://api.github.com/repos/google/episodes.dart/pulls{/number}",
-                "milestones_url": "https://api.github.com/repos/google/episodes.dart/milestones{/number}",
-                "notifications_url": "https://api.github.com/repos/google/episodes.dart/notifications{?since,all,participating}",
-                "labels_url": "https://api.github.com/repos/google/episodes.dart/labels{/name}",
-                "releases_url": "https://api.github.com/repos/google/episodes.dart/releases{/id}",
-                "deployments_url": "https://api.github.com/repos/google/episodes.dart/deployments",
-                "created_at": "2013-01-19T00:31:37Z",
-                "updated_at": "2019-09-23T11:53:58Z",
-                "pushed_at": "2014-10-09T21:39:33Z",
-                "git_url": "git://github.com/google/episodes.dart.git",
-                "ssh_url": "git@github.com:google/episodes.dart.git",
-                "clone_url": "https://github.com/google/episodes.dart.git",
-                "svn_url": "https://github.com/google/episodes.dart",
-                "homepage": None,
-                "size": 191,
-                "stargazers_count": 12,
-                "watchers_count": 12,
-                "language": "Dart",
-                "has_issues": True,
-                "has_projects": True,
-                "has_downloads": True,
-                "has_wiki": True,
-                "has_pages": False,
-                "forks_count": 22,
-                "mirror_url": None,
-                "archived": False,
-                "disabled": False,
-                "open_issues_count": 0,
                 "license": {
                     "key": "bsd-3-clause",
                     "name": "BSD 3-Clause \"New\" or \"Revised\" License",
                     "spdx_id": "BSD-3-Clause",
                     "url": "https://api.github.com/licenses/bsd-3-clause",
                     "node_id": "MDc6TGljZW5zZTU="
-                },
-                "forks": 22,
-                "open_issues": 0,
-                "watchers": 12,
-                "default_branch": "master",
-                "permissions": {
-                    "admin": False,
-                    "push": False,
-                    "pull": True
                 }
             },
             {
                 "id": 7776515,
                 "node_id": "MDEwOlJlcG9zaXRvcnk3Nzc2NTE1",
                 "name": "cpp-netlib",
-                "full_name": "google/cpp-netlib",
-                "private": False,
-                "owner": {
-                    "login": "google",
-                    "id": 1342004,
-                    "node_id": "MDEyOk9yZ2FuaXphdGlvbjEzNDIwMDQ=",
-                    "avatar_url": "https://avatars1.githubusercontent.com/u/1342004?v=4",
-                    "gravatar_id": "",
-                    "url": "https://api.github.com/users/google",
-                    "html_url": "https://github.com/google",
-                    "followers_url": "https://api.github.com/users/google/followers",
-                    "following_url": "https://api.github.com/users/google/following{/other_user}",
-                    "gists_url": "https://api.github.com/users/google/gists{/gist_id}",
-                    "starred_url": "https://api.github.com/users/google/starred{/owner}{/repo}",
-                    "subscriptions_url": "https://api.github.com/users/google/subscriptions",
-                    "organizations_url": "https://api.github.com/users/google/orgs",
-                    "repos_url": "https://api.github.com/users/google/repos",
-                    "events_url": "https://api.github.com/users/google/events{/privacy}",
-                    "received_events_url": "https://api.github.com/users/google/received_events",
-                    "type": "Organization",
-                    "site_admin": False
-                },
-                "html_url": "https://github.com/google/cpp-netlib",
-                "description": "The C++ Network Library Project -- header-only, cross-platform, standards compliant networking library.",
-                "fork": True,
-                "url": "https://api.github.com/repos/google/cpp-netlib",
-                "forks_url": "https://api.github.com/repos/google/cpp-netlib/forks",
-                "keys_url": "https://api.github.com/repos/google/cpp-netlib/keys{/key_id}",
-                "collaborators_url": "https://api.github.com/repos/google/cpp-netlib/collaborators{/collaborator}",
+                "license": {
+                    "key": "bsl-1.0",
+                    "name": "Boost Software License 1.0",
+                    "spdx_id": "BSL-1.0",
+                    "url": "https://api.github.com/licenses/bsl-1.0",
+                    "node_id": "MDc6TGljZW5zZTI4"
+                }
+            },
+            {
+                "id": 7968417,
+                "node_id": "MDEwOlJlcG9zaXRvcnk3OTY4NDE3",
+                "name": "dagger",
+                "license": {
+                    "key": "apache-2.0",
+                    "name": "Apache License 2.0",
+                    "spdx_id": "Apache-2.0",
+                    "url": "https://api.github.com/licenses/apache-2.0",
+                    "node_id": "MDc6TGljZW5zZTI="
+                }
+            },
+            {
+                "id": 8165161,
+                "node_id": "MDEwOlJlcG9zaXRvcnk4MTY1MTYx",
+                "name": "ios-webkit-debug-proxy",
+                "license": {
+                    "key": "other",
+                    "name": "Other",
+                    "spdx_id": "NOASSERTION",
+                    "url": None,
+                    "node_id": "MDc6TGljZW5zZTA="
+                }
+            },
+            {
+                "id": 8459994,
+                "node_id": "MDEwOlJlcG9zaXRvcnk4NDU5OTk0",
+                "name": "google.github.io",
+                "license": None
+            },
+            {
+                "id": 8566972,
+                "node_id": "MDEwOlJlcG9zaXRvcnk4NTY2OTcy",
+                "name": "kratu",
+                "license": {
+                    "key": "apache-2.0",
+                    "name": "Apache License 2.0",
+                    "spdx_id": "Apache-2.0",
+                    "url": "https://api.github.com/licenses/apache-2.0",
+                    "node_id": "MDc6TGljZW5zZTI="
+                }
+            },
+            {
+                "id": 8858648,
+                "node_id": "MDEwOlJlcG9zaXRvcnk4ODU4NjQ4",
+                "name": "build-debian-cloud",
+                "license": {
+                    "key": "other",
+                    "name": "Other",
+                    "spdx_id": "NOASSERTION",
+                    "url": None,
+                    "node_id": "MDc6TGljZW5zZTA="
+                }
+            },
+            {
+                "id": 9060347,
+                "node_id": "MDEwOlJlcG9zaXRvcnk5MDYwMzQ3",
+                "name": "traceur-compiler",
+                "license": {
+                    "key": "apache-2.0",
+                    "name": "Apache License 2.0",
+                    "spdx_id": "Apache-2.0",
+                    "url": "https://api.github.com/licenses/apache-2.0",
+                    "node_id": "MDc6TGljZW5zZTI="
+                }
+            },
+            {
+                "id": 9065917,
+                "node_id": "MDEwOlJlcG9zaXRvcnk5MDY1OTE3",
+                "name": "firmata.py",
+                "license": {
+                    "key": "apache-2.0",
+                    "name": "Apache License 2.0",
+                    "spdx_id": "Apache-2.0",
+                    "url": "https://api.github.com/licenses/apache-2.0",
+                    "node_id": "MDc6TGljZW5zZTI="
+                }
+            }
+        ],
+        "expected_repos": [
+            "episodes.dart",
+            "cpp-netlib",
+            "dagger",
+            "ios-webkit-debug-proxy",
+            "google.github.io",
+            "kratu",
+            "build-debian-cloud",
+            "traceur-compiler",
+            "firmata.py"
+        ],
+        "apache2_repos": ["dagger", "kratu", "traceur-compiler", "firmata.py"]
+    }
+])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Integration test case for GithubOrgClient class."""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Set up class by mocking requests.get with fixture payloads."""
+        cls.get_patcher = patch('requests.get')
+        cls.mock_get = cls.get_patcher.start()
+        def side_effect(url: str) -> Any:
+            class MockResponse:
+                def __init__(self, json_data: Dict):
+                    self.json_data = json_data
+                def json(self) -> Dict:
+                    return self.json_data
+            if url == cls.org_payload["repos_url"]:
+                return MockResponse(cls.repos_payload)
+            if url == f"https://api.github.com/orgs/{cls.org_name}":
+                return MockResponse(cls.org_payload)
+            return MockResponse({})
+        cls.mock_get.side_effect = side_effect
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Tear down class by stopping the requests.get patcher."""
+        cls.get_patcher.stop()
+
+    def test_public_repos(self) -> None:
+        """Test GithubOrgClient.public_repos returns expected repos."""
+        client = GithubOrgClient(self.org_name)
+        result = client.public_repos()
+        self.assertEqual(result, self.expected_repos)
+        self.mock_get.assert_any_call(f"https://api.github.com/orgs/{self.org_name}")
+        self.mock_get.assert_any_call(self.org_payload["repos_url"])
+
+    def test_public_repos_with_license(self) -> None:
+        """Test GithubOrgClient.public_repos with apache-2.0 license."""
+        with patch('utils.access_nested_map') as mock_access:
+            def side_effect(nested_map: Dict, path: tuple) -> Any:
+                try:
+                    return access_nested_map(nested_map, path)
+                except KeyError:
+                    return None
+            mock_access.side_effect = side_effect
+            client = GithubOrgClient(self.org_name)
+            result = client.public_repos(license="apache-2.0")
+            self.assertEqual(result, self.apache2_repos)
+            self.mock_get.assert_any_call(f"https://api.github.com/orgs/{self.org_name}")
+            self.mock_get.assert_any_call(self.org_payload["repos_url"])
+
+
+if __name__ == "__main__":
+    unittest.main()
