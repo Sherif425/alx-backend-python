fddb7ebf0219e39ff13c277b220a58b3407c8036
Sherif Mohamed <sherif425@gmail.com>
2025-07-28 13:35:37 +0300

    Adding views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+from django.db.models import Q
 
-# Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for listing and creating conversations.
+    """
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Return conversations where the requesting user is a participant.
+        """
+        user = self.request.user
+        return Conversation.objects.filter(participants=user)
+
+    def perform_create(self, serializer):
+        """
+        Create a new conversation and add the requesting user as a participant.
+        """
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for listing and sending messages in a conversation.
+    """
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Return messages for a specific conversation where the user is a participant.
+        """
+        user = self.request.user
+        conversation_id = self.kwargs.get('conversation_id')
+        if conversation_id:
+            return Message.objects.filter(
+                conversation__conversation_id=conversation_id,
+                conversation__participants=user
+            )
+        return Message.objects.filter(conversation__participants=user)
+
+    def perform_create(self, serializer):
+        """
+        Create a new message, setting the sender to the requesting user.
+        """
+        conversation_id = self.kwargs.get('conversation_id')
+        conversation = Conversation.objects.get(conversation_id=conversation_id)
+        serializer.save(sender=self.request.user, conversation=conversation)
