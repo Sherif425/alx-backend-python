10a919df2f77c917b0b3d371f6bb2bc9da20dfe2
Sherif Mohamed <sherif425@gmail.com>
2025-08-05 12:34:37 +0300

    Create  3-concurrent.py

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,33 @@
+# 2-concurrent_queries.py
+
+import asyncio
+import aiosqlite
+
+DB_PATH = "users.db"
+
+async def async_fetch_users():
+    async with aiosqlite.connect(DB_PATH) as db:
+        async with db.execute("SELECT * FROM users") as cursor:
+            users = await cursor.fetchall()
+            print("All users:")
+            for user in users:
+                print(user)
+            return users
+
+async def async_fetch_older_users():
+    async with aiosqlite.connect(DB_PATH) as db:
+        async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
+            older_users = await cursor.fetchall()
+            print("\nUsers older than 40:")
+            for user in older_users:
+                print(user)
+            return older_users
+
+async def fetch_concurrently():
+    await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
+
+if __name__ == "__main__":
+    asyncio.run(fetch_concurrently())
