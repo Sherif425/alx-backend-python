52fdcbcc7797ddad9c019a3bbcf34afc7a655fa4
Sherif Mohamed <sherif425@gmail.com>
2025-08-05 17:20:46 +0300

    changes in views.py and ursl.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,12 @@
-from rest_framework.routers import DefaultRouter
+from django.urls import path, include  # ✅ path, include
+from rest_framework.routers import DefaultRouter  # ✅ DefaultRouter()
+
 from .views import ConversationViewSet, MessageViewSet
 
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),  # ✅ path and include used
+]
